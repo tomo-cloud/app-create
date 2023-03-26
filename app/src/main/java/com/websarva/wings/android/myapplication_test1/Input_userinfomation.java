39f22e20945d6edbd644bbd47ee1ccee0843a33b
tomoya1107 <tomoya@greyandnavy.com>
2023-03-26 19:37:13 +0900

    新規プロジェクト作成

--- app/src/main/java/com/websarva/wings/android/myapplication_test1/Input_userinfomation.java
+++ app/src/main/java/com/websarva/wings/android/myapplication_test1/Input_userinfomation.java
@@ -3,18 +3,20 @@ package com.websarva.wings.android.myapplication_test1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Input_userinfomation extends AppCompatActivity {
-
+    private SharedPreferences data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_userinfomation);
-        //インテントオブジェクトの取得
+
+                //インテントオブジェクトの取得
         Intent intent=getIntent();
         Button next_button=findViewById(R.id.nextbutton2);
        HelloListener2 listener=new HelloListener2();
@@ -28,6 +30,14 @@ public class Input_userinfomation extends AppCompatActivity {
             EditText useraddress=findViewById(R.id.edituseraddress);
             String inputname=username.getText().toString();
             String inputaddress=useraddress.getText().toString();
+            // ファイル名:DataStore
+            SharedPreferences data = getSharedPreferences("DataStore", MODE_PRIVATE);
+            SharedPreferences.Editor editor = data.edit();
+            // Key: input, value: text
+            editor.putString("save-data", inputname);
+            editor.putString("save-data2",inputaddress);
+            //editor.commit();
+            editor.apply();
             Intent intent=new Intent(Input_userinfomation.this,selectplaceActivity.class);
             intent.putExtra("inputname",inputname);
             intent.putExtra("inputaddress",inputaddress);
