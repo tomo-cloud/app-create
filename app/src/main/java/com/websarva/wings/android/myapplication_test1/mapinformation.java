29c5ccce4f93d07d1d5725a2b7dc6a544b1a5553
tomoya1107 <tomoya@greyandnavy.com>
2023-04-01 17:43:10 +0900

    新規プロジェクト作成

--- app/src/main/java/com/websarva/wings/android/myapplication_test1/mapinformation.java
+++ app/src/main/java/com/websarva/wings/android/myapplication_test1/mapinformation.java
@@ -2,7 +2,11 @@ package com.websarva.wings.android.myapplication_test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class mapinformation extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class mapinformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapinformation);
+        Button Returnbutton=findViewById(R.id.Register);
+        MapReturn mapReturn=new MapReturn();
+        Returnbutton.setOnClickListener(mapReturn);
+    }
+    private class MapReturn implements View.OnClickListener {
+        public void onClick(View view) {
+            Intent intent=new Intent(mapinformation.this,toiletMapsActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
